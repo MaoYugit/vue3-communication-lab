32a6f681415e71e1dbd5de2f136acebc2d09ca56
maoyu <2874553847@qq.com>
2026-06-09 13:36:32 +0800

    feat: 为路由添加 meta 标题以改善页面标题管理

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,46 +4,55 @@ const routes = [
   {
     path: "/",
     name: "Home",
+    meta: { title: "Home" },
     component: () => import("../views/HomeView.vue"),
   },
   {
     path: "/props-emit",
     name: "Props & Emit",
+    meta: { title: "Props & Emit" },
     component: () => import("../views/Props/PropsEmitView.vue"),
   },
   {
     path: "/v-model",
     name: "v-model",
+    meta: { title: "v-model" },
     component: () => import("../views/V-model/VModelView.vue"),
   },
   {
     path: "/attrs",
     name: "$attrs",
+    meta: { title: "$attrs" },
     component: () => import("../views/Attrs/AttrsView.vue"),
   },
   {
     path: "/ref-expose",
     name: "ref & defineExpose",
+    meta: { title: "ref & defineExpose" },
     component: () => import("../views/RefExpose/RefExposeView.vue"),
   },
   {
     path: "/provide-inject",
     name: "Provide & Inject",
+    meta: { title: "Provide & Inject" },
     component: () => import("../views/ProvideInject/ProvideInjectView.vue"),
   },
   {
     path: "/pinia",
     name: "Pinia",
+    meta: { title: "Pinia" },
     component: () => import("../views/Pinia/PiniaView.vue"),
   },
   {
     path: "/mitt",
     name: "Mitt",
+    meta: { title: "Mitt" },
     component: () => import("../views/Mitt/MittView.vue"),
   },
   {
     path: "/slots",
     name: "Slots",
+    meta: { title: "Slots" },
     component: () => import("../views/Slot/SlotsView.vue"),
   },
 ];
@@ -51,6 +60,21 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach((to) => {
+  const pageTitle = to.meta.title;
+  const siteName = "vue3-communication-lab";
+
+  if (pageTitle) {
+    if (to.name === "Home") {
+      document.title = `${siteName}`;
+    } else {
+      document.title = `${pageTitle} | ${siteName}`;
+    }
+  } else {
+    document.title = siteName;
+  }
 });
 
 export default router;
